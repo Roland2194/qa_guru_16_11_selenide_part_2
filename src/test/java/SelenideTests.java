54ec3e5a58620eb61cd52fff722c0a800c24282a
egor.dobroskok <egor.dobroskok@simbirsoft.com>
2023-02-02 22:25:22 +0400

    added Github and drag&drop tests

---
+++ src/test/java/SelenideTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTests {
+
+    @Test
+    void checkEnterprisePage() {
+        open("https://github.com");
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(".h1-mktg").shouldHave(text("Build like the best"));
+    }
+
+
+    @Test
+    void dragAndDropElements() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
